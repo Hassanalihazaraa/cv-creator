9caebc850912536b76323a7fb02be0eea99769f5
Hassan <hassanalihazaraa@gmail.com>
2020-11-18 17:25:53 +0100

    added experience

--- main.py
+++ main.py
@@ -23,5 +23,18 @@ document.add_paragraph(
 )
 
 
+# work experience
+document.add_heading('Work Experience')
+p = document.add_paragraph()
+
+company = input('Enter a company name')
+from_date = input('From Date')
+to_date = input('To Date')
+
+p.add_run(company + ' ').bold = True
+p.add_run(from_date + '-' + to_date + '\n').italic = True
+
+experience_details = input('Describe your experience at ' + company)
+
 # save inside the document
 document.save("cv.docx")
