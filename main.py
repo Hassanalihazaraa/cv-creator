d24060729b1373a1621044bb1bedc5793d22df60
Hassan <hassanalihazaraa@gmail.com>
2020-11-18 17:19:20 +0100

    added a heading about me

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 from docx import Document
+from docx.shared import Inches
 
 document = Document()
 
-document.add_picture('img.jpg')
+# Add profile picture
+document.add_picture('img.jpg', width=Inches(2.0))
 
+# inputs from the user
 name = input('What is your name? ')
 phone_number = input(' What is your phone number? ')
 email = input('What is your email address? ')
 
+# adding all in paragraph
 document.add_paragraph(
-    name + ' | ' + phone_number + ' | ' + email
+    name + '\n' + phone_number + '\n' + email
 )
 
+# about me
+document.add_heading('About me')
+document.add_paragraph(
+    input("Tell me about yourself? ")
+)
+
+
+# save inside the document
 document.save("cv.docx")
