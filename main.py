74fb522a1a1ee6f7105ccc9d14eab8ad7e20a234
Hassan <hassanalihazaraa@gmail.com>
2020-11-19 11:42:15 +0100

    done with creating cv

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from docx import Document
 from docx.shared import Inches
+import pyttsx3
+
+
+def speak(text):
+    pyttsx3.speak(text)
+
 
 document = Document()
 
@@ -8,6 +14,7 @@ document.add_picture('img.jpg', width=Inches(2.0))
 
 # inputs from the user
 name = input('What is your name? ')
+
 phone_number = input(' What is your phone number? ')
 email = input('What is your email address? ')
 
@@ -27,14 +34,54 @@ document.add_paragraph(
 document.add_heading('Work Experience')
 p = document.add_paragraph()
 
-company = input('Enter a company name')
-from_date = input('From Date')
-to_date = input('To Date')
+company = input('Enter a company name ')
+from_date = input('From Date ')
+to_date = input('To Date ')
 
 p.add_run(company + ' ').bold = True
 p.add_run(from_date + '-' + to_date + '\n').italic = True
 
-experience_details = input('Describe your experience at ' + company)
+experience_details = input('Describe your experience at ' + company + ' ')
+
+# more experiences
+while True:
+    has_more_experiences = input('Do you have more experiences? Yes or No ')
+    if has_more_experiences.lower() == 'yes':
+        p = document.add_paragraph()
+        company = input('Enter a company name ')
+        from_date = input('From Date ')
+        to_date = input('To Date ')
+
+        p.add_run(company + ' ').bold = True
+        p.add_run(from_date + '-' + to_date + '\n').italic = True
+
+        experience_details = input(
+            'Describe your experience at ' + company + ' ')
+    else:
+        break
+
+
+# Skills
+document.add_heading('Skills')
+skill = input('Enter a skill ')
+p = document.add_paragraph(skill)
+p.style = 'List Bullet'
+
+while True:
+    has_more_skills = input('Do you have more skills? Yes or No ')
+    if has_more_skills.lower() == 'yes':
+        skill = input('Enter a skill ')
+        p = document.add_paragraph(skill)
+        p.style = 'List Bullet'
+    else:
+        break
+
+
+# footer
+section = document.sections[0]
+footer = section.footer
+p = footer.paragraphs[0]
+p.text = 'CV generated using Hassan"s app'
 
 # save inside the document
 document.save("cv.docx")
