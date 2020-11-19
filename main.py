aad1ccd3844e5d26d860df6c42efb9bd33837847
Hassan <hassanalihazaraa@gmail.com>
2020-11-19 12:06:07 +0100

    added text-to-speech

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from docx import Document
 from docx.shared import Inches
 import pyttsx3
 
+voice_engine = pyttsx3.init()
+new_volume = 0.1
+new_voice_rate = 180
+
 
 def speak(text):
+    voice_engine.setProperty('volume', new_volume)
+    voice_engine.setProperty('rate', new_voice_rate)
     pyttsx3.speak(text)
 
 
@@ -13,9 +19,11 @@ document = Document()
 document.add_picture('img.jpg', width=Inches(2.0))
 
 # inputs from the user
+speak('Hi, May i ask your name?')
 name = input('What is your name? ')
-
+speak('Hi' + name + ' nice to meet you, What is your phone number?')
 phone_number = input(' What is your phone number? ')
+speak('What is your email address?')
 email = input('What is your email address? ')
 
 # adding all in paragraph
@@ -25,6 +33,7 @@ document.add_paragraph(
 
 # about me
 document.add_heading('About me')
+speak("Tell me about yourself? ")
 document.add_paragraph(
     input("Tell me about yourself? ")
 )
@@ -34,6 +43,7 @@ document.add_paragraph(
 document.add_heading('Work Experience')
 p = document.add_paragraph()
 
+speak('Please enter your work experience')
 company = input('Enter a company name ')
 from_date = input('From Date ')
 to_date = input('To Date ')
@@ -41,11 +51,12 @@ to_date = input('To Date ')
 p.add_run(company + ' ').bold = True
 p.add_run(from_date + '-' + to_date + '\n').italic = True
 
+speak('Describe your experience at ' + company + ' ')
 experience_details = input('Describe your experience at ' + company + ' ')
-
 # more experiences
 while True:
     has_more_experiences = input('Do you have more experiences? Yes or No ')
+    speak(has_more_experiences + 'if yes enter it below else just write no')
     if has_more_experiences.lower() == 'yes':
         p = document.add_paragraph()
         company = input('Enter a company name ')
@@ -64,11 +75,13 @@ while True:
 # Skills
 document.add_heading('Skills')
 skill = input('Enter a skill ')
+speak('Please enter all your skills')
 p = document.add_paragraph(skill)
 p.style = 'List Bullet'
 
 while True:
     has_more_skills = input('Do you have more skills? Yes or No ')
+    speak(has_more_skills + 'if yes enter it below else just write no')
     if has_more_skills.lower() == 'yes':
         skill = input('Enter a skill ')
         p = document.add_paragraph(skill)
